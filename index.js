fc01a96811c6a879a8a53b105771a56c71dcdbfc
unknown <fatmaelbastawy@gmail.com>
2023-05-05 15:24:23 +0300

    day2

---
+++ index.js
@@ -0,0 +1,6 @@
+// var container=document.getElementsByClassName("container");
+// var x=document.getElementsByClassName("close button");
+// function closeChip(){
+//     container.style.width=0
+// }
+
